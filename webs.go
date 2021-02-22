9bf9d6f4949d5cfcb33708499b295d823de48b24
JiaFeiX <lixun7@gmailcom>
2021-02-22 16:09:18 +0800

    支持更换端口号

--- webs.go
+++ webs.go
@@ -2,12 +2,27 @@ package main
 
 import "net/http"
 import "fmt"
+import "flag"
 
 func main() {
+
+	defaultPortPtr := flag.String("p", "", "Port Number")
+	
+	flag.Parse()
+	
+	portNum := "9090"
+	
+	// Port Number
+	if *defaultPortPtr != "" {
+		portNum = *defaultPortPtr
+	} else {
+		portNum = "9090"
+	}
+
 	fmt.Println()
-	fmt.Println("HTTP Server On :9090 Started .")
+	fmt.Println("HTTP Server On :" + portNum + " Started .")
 	fmt.Println()
 	fmt.Println("Ver 1.0.0")
-	panic(http.ListenAndServe(":9090", http.FileServer(http.Dir("./"))))
+	panic(http.ListenAndServe(":"+portNum, http.FileServer(http.Dir("./"))))
 }
 
